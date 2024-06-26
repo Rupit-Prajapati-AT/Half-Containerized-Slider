c3fb1d35cb2508502dea1811ce4bb19cb70460d5
Rupit Prajapati <113025099+Rupit-Prajapati-AT@users.noreply.github.com>
2024-06-26 15:35:57 +0530

    slider created with responsive

---
+++ custom.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+     $(".slick-slider").slick({
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: '11%',
+          dots: false,
+          arrows: false,
+          responsive: [
+               {
+                    breakpoint: 992,
+                    settings: {
+                         slidesToShow: 3,
+                         centerPadding: '15%',
+                    }
+               },
+               {
+                    breakpoint: 768,
+                    settings: {
+                         slidesToShow: 2,
+                         centerPadding: '20%',
+                    }
+               },
+               {
+                    breakpoint: 576,
+                    settings: {
+                         slidesToShow: 1,
+                         centerPadding: '33%',
+                    }
+               },
+          ]
+     });
+})
